5f8997b79dfecca2afa819fd99829277c6405eec
Lokleee <2152210748@qq.com>
2024-10-15 16:14:00 +0800

    add new topic hover thrust estimate

--- common/testsuite.h
+++ common/testsuite.h
@@ -4266,6 +4266,66 @@ static void mavlink_test_omni_attitude_target(uint8_t system_id, uint8_t compone
 #endif
 }
 
+static void mavlink_test_hover_thrust_estimate(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
+{
+#ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
+    mavlink_status_t *status = mavlink_get_channel_status(MAVLINK_COMM_0);
+        if ((status->flags & MAVLINK_STATUS_FLAG_OUT_MAVLINK1) && MAVLINK_MSG_ID_HOVER_THRUST_ESTIMATE >= 256) {
+            return;
+        }
+#endif
+    mavlink_message_t msg;
+        uint8_t buffer[MAVLINK_MAX_PACKET_LEN];
+        uint16_t i;
+    mavlink_hover_thrust_estimate_t packet_in = {
+        963497464,45.0
+    };
+    mavlink_hover_thrust_estimate_t packet1, packet2;
+        memset(&packet1, 0, sizeof(packet1));
+        packet1.time_boot_ms = packet_in.time_boot_ms;
+        packet1.thrust = packet_in.thrust;
+        
+        
+#ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
+        if (status->flags & MAVLINK_STATUS_FLAG_OUT_MAVLINK1) {
+           // cope with extensions
+           memset(MAVLINK_MSG_ID_HOVER_THRUST_ESTIMATE_MIN_LEN + (char *)&packet1, 0, sizeof(packet1)-MAVLINK_MSG_ID_HOVER_THRUST_ESTIMATE_MIN_LEN);
+        }
+#endif
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_hover_thrust_estimate_encode(system_id, component_id, &msg, &packet1);
+    mavlink_msg_hover_thrust_estimate_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_hover_thrust_estimate_pack(system_id, component_id, &msg , packet1.time_boot_ms , packet1.thrust );
+    mavlink_msg_hover_thrust_estimate_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_hover_thrust_estimate_pack_chan(system_id, component_id, MAVLINK_COMM_0, &msg , packet1.time_boot_ms , packet1.thrust );
+    mavlink_msg_hover_thrust_estimate_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+        mavlink_msg_to_send_buffer(buffer, &msg);
+        for (i=0; i<mavlink_msg_get_send_buffer_length(&msg); i++) {
+            comm_send_ch(MAVLINK_COMM_0, buffer[i]);
+        }
+    mavlink_msg_hover_thrust_estimate_decode(last_msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+        
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_hover_thrust_estimate_send(MAVLINK_COMM_1 , packet1.time_boot_ms , packet1.thrust );
+    mavlink_msg_hover_thrust_estimate_decode(last_msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+#ifdef MAVLINK_HAVE_GET_MESSAGE_INFO
+    MAVLINK_ASSERT(mavlink_get_message_info_by_name("HOVER_THRUST_ESTIMATE") != NULL);
+    MAVLINK_ASSERT(mavlink_get_message_info_by_id(MAVLINK_MSG_ID_HOVER_THRUST_ESTIMATE) != NULL);
+#endif
+}
+
 static void mavlink_test_set_position_target_local_ned(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
 {
 #ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
@@ -14387,6 +14447,7 @@ static void mavlink_test_common(uint8_t system_id, uint8_t component_id, mavlink
     mavlink_test_attitude_target(system_id, component_id, last_msg);
     mavlink_test_set_omni_attitude_target(system_id, component_id, last_msg);
     mavlink_test_omni_attitude_target(system_id, component_id, last_msg);
+    mavlink_test_hover_thrust_estimate(system_id, component_id, last_msg);
     mavlink_test_set_position_target_local_ned(system_id, component_id, last_msg);
     mavlink_test_position_target_local_ned(system_id, component_id, last_msg);
     mavlink_test_set_position_target_global_int(system_id, component_id, last_msg);
